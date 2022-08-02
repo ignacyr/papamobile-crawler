c798f06241d388f9fc567aa9abed3d893f2d36aa
ignacyr <ignacy.rogatty@gmail.com>
2022-08-02 20:30:25 +0200

    title and date and time works

--- papamobile/spiders/otomoto.py
+++ papamobile/spiders/otomoto.py
@@ -1,4 +1,6 @@
 import scrapy
+from datetime import datetime
+
 
 class OtomotoSpider(scrapy.Spider):
     name = "otomoto"
@@ -18,4 +20,16 @@ class OtomotoSpider(scrapy.Spider):
                 yield scrapy.Request(url=url, callback=self.parse_cars)
 
     def parse_cars(self, response):
-        
+        time_date = response.xpath('//span[@class="offer-meta__value"]/text()').get()
+        months_map = {"stycznia": "01", "lutego": "02", "marca": "03",
+                      "kwietnia": "04", "maja": "05", "czerwca": "06",
+                      "lipca": "07", "sierpnia": "08", "września": "09",
+                      "października": "10", "listopada": "11", "grudnia": "12"}
+        for month in months_map.keys():
+            time_date = time_date.replace(month, months_map[month])
+        time_date = datetime.strptime(time_date, '%H:%M, %d %m %Y')
+
+        title = response.xpath('//h1[@class="offer-title big-text"]/text()').getall()[-1].strip()
+        title = ' '.join(title.split())
+
+        print(title, time_date)
