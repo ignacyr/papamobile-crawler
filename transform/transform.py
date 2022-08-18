d943d589a8e396edc38bcef8926ee7a2f44bee71
EC2 Default User <ec2-user@ip-172-31-44-251.eu-central-1.compute.internal>
2022-08-18 19:22:21 +0000

    now its not downloading all data, but only yesterdays data

--- transform/transform.py
+++ transform/transform.py
@@ -3,13 +3,17 @@ import pandas as pd
 from datetime import datetime, date, timedelta
 
 if __name__ == "__main__":
-    all = requests.get("http://34.141.144.103:8000/base/").json()
-    all_df = pd.DataFrame.from_dict(all)
+    #all = requests.get("http://34.141.144.103:8000/base/").json()
+    #all_df = pd.DataFrame.from_dict(all)
     
     yesterday = date.today() - timedelta(days=1)
     yesterday = yesterday.strftime("%Y-%m-%dT00:00:00Z")
     
-    yest_cars_df = all_df[all_df["added"] == yesterday]
+    #yest_cars_df = all_df[all_df["added"] == yesterday]
+    
+    yest_cars = requests.get(f"http://34.141.144.103:8000/base/date_list?date={yesterday}").json()
+    yest_cars_df = pd.DataFrame.from_dict(yest_cars)
+    
     avg_price = round(yest_cars_df["price"].median())
     
     resp_avg = requests.post("http://34.141.144.103:8000/base/daily_avg", {"date": yesterday, "price": avg_price, "count": len(yest_cars_df)})
